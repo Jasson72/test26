2d1cc8ec22017ca868ddf2398a4a3fbf066f400b
Jasson72 <145915046+Jasson72@users.noreply.github.com>
2024-04-16 01:46:27 +0700

    Add files via upload

---
+++ word.js
@@ -0,0 +1,28 @@
+let wordList = [
+    {
+        word: "bystander",
+        hint: "sebutan saksi dalam bullying"
+    },
+    {
+        word: "bully",
+        hint: "kekerasan yang dilakukan berkali-kali"
+    },
+    {
+        word: "kekerasan",
+        hint: "ketersinggungan yang hanya 1 atau 2 kali"
+    },
+    {
+        word: "fisik",
+        hint: "jenis bully yang menyebabkan luka-luka"
+    },
+    {
+        word: "mental",
+        hint: "jenis bully yang menyebabkan kerusakan psikis"
+    },
+    {
+        word: "digital",
+        hint: "dunia cyberbullying"
+    },
+    
+]
+
